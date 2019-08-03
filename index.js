bf5b10756125116313f115af832e8412594de5bc
George <starcalluh@live.com>
2019-08-03 08:19:50 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,39 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) 
+    {
+  console.log(string.toUpperCase()) 
+}
+
+function logWhisper(string)
+{
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var GlowerCase = "I can't hear you!" ;
+  var GupperCase = "YES INDEED!" ;
+  var Glov ="I love you, too.";
+  if (string.toLowerCase() === !") 
+  {
+  return GlowerCase ; 
+  }
+    else if (string.toUpperCase())
+  {return GupperCase
+  }
+  else if ("I love you, Grandma.") {
+    return Glov
+  }
+}
+  
+
+
+
+
+
+
